d3b732f70d6e607f025b6233722e4bbb1326f2a1
Mugil99 <124775550+Mugil99@users.noreply.github.com>
2023-03-19 14:41:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your code here
-
+let bandNames=["The Beatles","Rolling Stones", "Led Zeppelin","Pink Floyd","The Who","The Doors"];
+//to remove articles
+function removeArticle(bandName) {
+	let articles=["a","an","the"];
+	let words=bandName.split(" ");
+	if(articles.includes(words[0].toLowerCase())){
+		words.shift();
+	}
+	return words.join(" ");
+}
+bandNames.sort(function (a,b) {
+	return removeArticle(a).localeCompare(removeArticle(b));
+});
+let ulElement=document.getElementById("band");
+for(let i=0;i<bandNames.length;i++){
+	let liElement=document.createElement("li");
+	liElement.textContent=bandNames[i];
+	ulElement.appendChild(liElement);
+}
